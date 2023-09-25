eaed9ca0adfd6d466adc96b00523bb347b2a447d
Canadea <noah.canadea@cmiag.ch>
2023-09-25 14:54:04 +0200

    refac(collector_container): Make collector container more flexible by allowing to enable and disable features

--- README.md
+++ README.md
@@ -6,32 +6,42 @@ Das folgende Projekt enthält alle Resourcen, die im Zusammenhang mit dem Grafan
 
 Im Ordner `grafana_collector_container` befinden sich das Projekt für den Grafana Collector, der in jeder Azure Umgebung (CMI Cloud Prod, CMI Cloud Stage usw.) einmal läuft. Dieser Collector wird als Container gestartet und findet alle VMs in der angegebenen Azure Subscription. Die Authentifizierung bei Azure erfolgt dabei über eine Managed Identity der Container Instance.
 
-### Konfiguration
+### Allgemeine Konfiguration
 
-Der Container lässt sich vollständig per Env-Variablen konfigurieren. Die folgenden Variablen sind verfügbar:
+Der Container lässt sich vollständig per Env-Variablen konfigurieren. Die folgenden Variablen sind bezüglich allgemeiner Konfiguration verfügbar:
 
-| Name                  | default | Pflicht | Beschreibung                                                                                               |
-| --------------------- | :------ | ------- | ---------------------------------------------------------------------------------------------------------- |
-| GRAFANA_TOKEN         | -       | Ja      | Grafana token für die Authentifizierung bei Grafana Cloud                                                  |
-| SITE_NAME             | -       | Ja      | Name der Site in dem der Collector betreiben wird (Bspw. CMI Cloud Prod oder UMB).                         |
-| AGENT_NAME            | -       | Ja      | Name des Collector-Agent.                                                                                  |
-| AZURE_CLIENT_ID       | -       | Ja      | Client ID der Azure managed identity.                                                                      |
-| AZURE_SUBSCRIPTION_ID | -       | Ja      | Subscription-ID, die der Collector überwachen soll.                                                        |
-| AZURE_ENV_NAME        | -       | Ja      | Name des Azure Env. (Bspw. Prod oder Stage).                                                               |
-| STACK_NAME            | null    | Nein    | Name des Grafana Cloud Stack. Wenn nicht angegeben, wird der default Stack aus dem Uplink-Modul verwendet. |
-| BRANCH_NAME           | master  | Nein    | Branch von welchem die verwendeten River-Module abgerufen werden sollen.                                   |
-| LOG_LEVEL             | info    | Nein    | Log-Level des Collector-Agent.                                                                             |
+| Name                          | default      | Pflicht | Beschreibung                                                                                               |
+| ----------------------------- | :----------- | ------- | ---------------------------------------------------------------------------------------------------------- |
+| GRAFANA_TOKEN                 | -            | Ja      | Grafana token für die Authentifizierung bei Grafana Cloud                                                  |
+| SITE_NAME                     | -            | Ja      | Name der Site in dem der Collector betreiben wird (Bspw. CMI Cloud Prod oder UMB).                         |
+| AGENT_NAME                    | -            | Ja      | Name des Collector-Agent.                                                                                  |
+| STACK_NAME                    | cminformatik | Nein    | Name des Grafana Cloud Stack. Wenn nicht angegeben, wird der default Stack aus dem Uplink-Modul verwendet. |
+| BRANCH_NAME                   | master       | Nein    | Branch von welchem die verwendeten River-Module abgerufen werden sollen.                                   |
+| LOG_LEVEL                     | info         | Nein    | Log-Level des Collector-Agent.                                                                             |
+| ENABLE_OPENTELEMETRY_RECEIVER | 1            | Nein    | Soll der OpenTelemetry Receiver aktiviert werden? 0 = Nein, 1 = Ja.                                        |
+| ENABLE_AZURE_AUTODISCOVERY    | 1            | Nein    | Soll die Azure Auto-Discovery Integration aktiviert werden? 0 = Nein, 1 = Ja.                              |
 
 ### OTEL-Collector
 
-Der Collector stellt eine Opentelemetry-Schnittstelle bereit. Diese hört auf den Ports 4317 (OTLP-GRPC) und 4318 (OTLP-HTTP). Die Schnittstelle kann genutzt werden, um Metriken, Logs und Traces an den Collector zu senden. Die Daten werden dann verarbeitet (filtering und tagging) und an den konfigurierten Grafana Cloud Stack gesendet.
+Wird `ENABLE_OPENTELEMETRY_RECEIVER = 1` gesetzt, wird ein OpenTelemetry Receiver konfiguriert und gestartet. Diese hört auf den Ports 4317 (OTLP-GRPC) und 4318 (OTLP-HTTP). Die Schnittstelle kann genutzt werden, um Metriken, Logs und Traces an den Collector zu senden. Die Daten werden dann verarbeitet (filtering und tagging) und an den konfigurierten Grafana Cloud Stack gesendet.
+
+### Azure Auto-Discorvery
+
+Wird `ENABLE_AZURE_AUTODISCOVERY = 1` gesetz, stehen ausserdem die folgenden Einstellungen zur Verfügung:
+
+| Name                  | default | Pflicht | Beschreibung                                        |
+| --------------------- | :------ | ------- | --------------------------------------------------- |
+| AZURE_CLIENT_ID       | -       | Ja      | Client ID der Azure managed identity.               |
+| AZURE_SUBSCRIPTION_ID | -       | Ja      | Subscription-ID, die der Collector überwachen soll. |
+| AZURE_ENV_NAME        | -       | Ja      | Name des Azure Env. (Bspw. Prod oder Stage).        |
 
 ### Testen des Collectors
 
-Für die Secrets muss im Ordner `grafana_collector_container` ein Secrets file mit dem Namen `agent_secrets.env` und folgenden Inhalt angelegt werden:
+Für die Secrets muss im Ordner `grafana_collector_container` ein Secrets file mit dem Namen `local_configuration.env` und folgenden Inhalt angelegt werden:
 
 ```bash
 GRAFANA_TOKEN=<grafana_token>
+BRANCH_NAME=<branch_name>
 ```
 
 Um den Collector lokal zu testen, kann diser anschliessend mit dem folgenden Befehl gebaut und gestartet werden:
--- grafana_collector_container/entrypoint.sh
+++ grafana_collector_container/entrypoint.sh
@@ -1,57 +1,121 @@
 #!/bin/bash
 
-############################# CMI Cloud Azure Grafana Agent Entry Point #####################################
+##################################################### CMI Cloud Azure Grafana Agent Entry Point #####################################################
 # Das folgende Script dient als container entry point und kümmert sich um die einrichtung des Grafana Agent anhand der übergebenen Env Variablen
 
 grafanaAgentConfigPath="/etc/agent/config.river"
 
-function replaceGrafanaAgentConfigValue() {
-    local envVarName=$1
-    local configKey=$2
-    local requred=$3
-    local sensitive=$4
-    local defaultValue=$5
-    local envVarValue=${!envVarName}
-    
-    if [ -z "$envVarValue" ]; then
-        if [ "$requred" = 1 ]; then
-            echo "Environment variable $envVarName is required, please specify a value. Exiting..."
+function handle_env_variable() {
+    env_variable_name=$1
+    default_value=$2
+    env_variable_value=${!env_variable_name}
+    if [ -z "$env_variable_value" ]; then
+        if [ -z "$default_value" ]; then
+            echo "Environment variable $env_variable_name is required but not set. Please specify a value. Exiting..."
             exit 1
+        else
+            echo "Environment variable $env_variable_name is not set. Using default value '$default_value'"
+            export "$env_variable_name"="$default_value"
         fi
-        if [ -z "$defaultValue" ]; then
-            echo "Environment variable $envVarName is not set. Removing config key $configKey from grafana config file..."
-            sed -i "s|${configKey}|d" $grafanaAgentConfigPath
-            return
-        fi
-        echo "Environment variable $envVarName is not set. Using default value $defaultValue"
-        sed -i "s|${configKey}|${defaultValue}|g" $grafanaAgentConfigPath
-        return
     fi
-    envVarValueLogValue=$envVarValue
-    if [ "$sensitive" = 1 ]; then
-        envVarValueLogValue="${envVarValue:0:4}..."
-    fi
-    configValue=${!envVarName}
-    echo "Replacing config value for $configKey with $envVarValueLogValue"
-    sed -i "s|${configKey}|${configValue}|g" $grafanaAgentConfigPath
 }
 
-# Entry Point
-echo "Reading environment variables and setting Grafana Agent configuration..."
+# Check required environment variables and set default values if not set
+handle_env_variable "GRAFANA_TOKEN"
+handle_env_variable "SITE_NAME"
+handle_env_variable "AGENT_NAME"
+handle_env_variable "STACK_NAME" "cminformatik"
+handle_env_variable "BRANCH_NAME" "master"
+handle_env_variable "LOG_LEVEL" "info"
+handle_env_variable "ENABLE_OPENTELEMETRY_RECEIVER" 1
+handle_env_variable "ENABLE_AZURE_AUTODISCOVERY" 1
 
-# Get all required environment variables and set the values in the grafana agent config
-replaceGrafanaAgentConfigValue "GRAFANA_TOKEN" "<replace_grafana_token>" 1 1
-replaceGrafanaAgentConfigValue "SITE_NAME" "<replace_site_name>" 1 0
-replaceGrafanaAgentConfigValue "AGENT_NAME" "<replace_agent_name>" 1 0
-replaceGrafanaAgentConfigValue "AZURE_CLIENT_ID" "<replace_azure_client_id>" 1 0
-replaceGrafanaAgentConfigValue "AZURE_SUBSCRIPTION_ID" "<replace_azure_subscription_id>" 1 0
-replaceGrafanaAgentConfigValue "AZURE_ENV_NAME" "<replace_azure_env_name>" 1 0
+if [ -f "$grafanaAgentConfigPath" ]; then
+    # Remove default config file if it exists inside the container
+    rm $grafanaAgentConfigPath
+fi
+touch $grafanaAgentConfigPath
+
+echo "Setting Grafana Agent base configuration with the following values:"
+echo "GRAFANA_TOKEN: ${GRAFANA_TOKEN:0:4}..."
+echo "SITE_NAME: $SITE_NAME"
+echo "AGENT_NAME: $AGENT_NAME"
+echo "STACK_NAME: $STACK_NAME"
+echo "BRANCH_NAME: $BRANCH_NAME"
+cat << EOF >> $grafanaAgentConfigPath
+logging {
+	level = "$LOG_LEVEL"
+}
+
+module.git "base_module" {
+	repository = "https://github.com/CMInformatik/cmi-grafana-monitoring.git"
+	path       = "modules/grafana_agent_uplink/module.river"
+	revision   = "$BRANCH_NAME"
+
+	arguments {
+		token            = "$GRAFANA_TOKEN"
+		site             = "$SITE_NAME"
+		stack_name       = "$STACK_NAME"
+		submodule_branch = "$BRANCH_NAME"
+	}
+}
+
+prometheus.scrape "grafana_agent" {
+	targets    = [{"__address__" = "localhost:12345", "job" = "integrations/agent", "instance" = "$AGENT_NAME"}]
+	forward_to = [
+		module.git.base_module.exports.metrics_receiver,
+	]
+}
+EOF
+
+echo "Grafana Agent base configuration set."
 
 
-# Get all optional environment variables and set the values in the grafana agent config. If the value is not set, the config line will be removed
-replaceGrafanaAgentConfigValue "STACK_NAME" "<replace_stack_name>" 0 0
-replaceGrafanaAgentConfigValue "BRANCH_NAME" "<replace_branch_name>" 0 0 "master"
-replaceGrafanaAgentConfigValue "LOG_LEVEL" "<replace_log_level>" 0 0 "info"
+# check and setup azure auto discovery for vms
+if  [  "$ENABLE_AZURE_AUTODISCOVERY" == 1 ]; then
+    echo "Azure auto-discovery enabled, checking requirements..."
+    
+    handle_env_variable "AZURE_CLIENT_ID"
+    handle_env_variable "AZURE_SUBSCRIPTION_ID"
+    handle_env_variable "AZURE_ENV_NAME"
+    
+    echo "Saving Azure Auto Discovery configuration with the following values:"
+    echo "AZURE_CLIENT_ID: $AZURE_CLIENT_ID"
+    echo "AZURE_SUBSCRIPTION_ID: $AZURE_SUBSCRIPTION_ID"
+    echo "AZURE_ENV_NAME: $AZURE_ENV_NAME"
+cat << EOF >> $grafanaAgentConfigPath
+module.file "azure_auto_discovery" {
+	filename  = "/etc/agent/submodules/azure_discovery.river"
+	arguments {
+		azure_subscription_id = "$AZURE_CLIENT_ID"
+		azure_client_id = "$AZURE_SUBSCRIPTION_ID"
+        azure_env_name = "$AZURE_ENV_NAME"
+		metrics_receiver = module.git.base_module.exports.metrics_receiver
+	}
+}
+EOF
+    echo "Azure Auto Discovery configuration set."
+else
+    echo "Azure Auto discovery configuration not set, skipping..."
+fi
+
+
+# add open telemetry receiver if not disabled
+if [  "$ENABLE_OPENTELEMETRY_RECEIVER" == 1 ]; then
+    echo "Configuring Open Telemetry Receiver..."
+cat << EOF >> $grafanaAgentConfigPath
+module.file "otelcol" {
+	filename  = "/etc/agent/submodules/otelcol.river"
+	arguments {
+		base_module_exports = module.git.base_module.exports
+		branch = "$BRANCH_NAME"
+	}
+}
+EOF
+    echo "Open Telemetry Receiver configured."
+else
+    echo "Open Telemetry Receiver configuration disabled."
+fi
 
 
 # Set Environment Variable AGENT_MODE to flow since we want to Run the Agent in Flow Mode
